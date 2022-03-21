e35a8879225628e2f4e9b1493a9fac061d663bb8
alex <a99som@yandex.ru>
2022-03-22 02:41:53 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <thread>
+#include "win_shortcuts.h"
+#include <chrono>
+
+
+int main() {
+//    bool err = FreeConsole();
+//    using namespace std::chrono_literals;
+//    LPSTR working_dir = "C:\\StorageUp\\StorageUp\\";
+//    LPSTR storageup_dir_path = "C:\\StorageUp\\StorageUp\\storageup.exe";
+//    int show_mode = SW_SHOWMAXIMIZED;
+//    LPSTR icon_path = "C:\\Users\\a99so\\Desktop\\src\\src\\StorageUp.ico";
+//    WinShortCut win_api;
+//    int icon_index = 0;
+//    LPSTR description = "description";
+//    win_api.createShortCut(storageup_dir_path, "",
+//                           "C:\\Users\\a99so\\Desktop\\StorageUp.lnk", description, show_mode,
+//                           working_dir, icon_path, icon_index);
+//    std::this_thread::sleep_for(2000ms);
+    system("start /min C:\\Users\\a99so\\CLionProjects\\win_api\\cmake-build-debug\\1.exe");
+}
---
+++ win_shortcuts.h
@@ -0,0 +1,33 @@
+//
+// Created by a99so on 21.03.2022.
+//
+
+#ifndef WIN_API_WIN_SHORTCUTS_H
+#define WIN_API_WIN_SHORTCUTS_H
+
+#include <string>
+#include <utility>
+#include <windows.h>
+#include <objbase.h>
+#include <shobjidl.h>
+#include <winnls.h>
+#include <objidl.h>
+#include <shlguid.h>
+
+
+class WinShortCut {
+
+public:
+    const std::string app_folder_path;
+    const std::string storageup_path;
+
+    WinShortCut() = default;
+    ~WinShortCut() = default;
+
+    [[maybe_unused]] bool createShortCut(LPSTR  path_to_exe, LPSTR args, LPSTR  path_to_shortcut,
+                                         LPSTR  description, int show_mode,
+                                         LPSTR  working_dir, LPSTR  icon_path, int icon_index);
+
+};
+
+#endif //WIN_API_WIN_SHORTCUTS_H
