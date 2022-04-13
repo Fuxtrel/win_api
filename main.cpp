e97f6fb802e8adda142f84756ff28654e9a6326e
alex <a99som@yandex.ru>
2022-04-14 02:40:04 +0300

    dfgndFnmfsm

--- main.cpp
+++ main.cpp
@@ -2,9 +2,24 @@
 #include <thread>
 #include "win_shortcuts.h"
 #include <chrono>
+#include <psapi.h>
+#include <winuser.h>
+#include <processthreadsapi.h>
 
+
+
+bool createProcess(){
+    std::string exe_path = R"(C:\StorageUp\StorageUp\storageup.exe)";
+    std::string working_dir_path = R"(C:\StorageUp\StorageUp\)";
+    return CreateProcessA(exe_path, nullptr, nullptr, nullptr, false, NORMAL_PRIORITY_CLASS, nullptr, working_dir_path, );
+}
 
 int main() {
+
+    unsigned int start_time = clock();
+    std::cout.flush() << "Hello" << std::endl;
+    unsigned int end_time = clock();
+    std::cout << "Time spent " << end_time - start_time;
 //    bool err = FreeConsole();
 //    using namespace std::chrono_literals;
 //    LPSTR working_dir = "C:\\StorageUp\\StorageUp\\";
@@ -18,5 +33,5 @@ int main() {
 //                           "C:\\Users\\a99so\\Desktop\\StorageUp.lnk", description, show_mode,
 //                           working_dir, icon_path, icon_index);
 //    std::this_thread::sleep_for(2000ms);
-    system("start /min C:\\Users\\a99so\\CLionProjects\\win_api\\cmake-build-debug\\1.exe");
+//    system("start /min C:\\Users\\a99so\\CLionProjects\\win_api\\cmake-build-debug\\1.exe");
 }
