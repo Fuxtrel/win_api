f52fcccb7e81773a7a33c5304be07813c16795c2
a.som <Mechtatel1999>
2022-04-14 18:21:45 +0300

    sdcsdc

--- main.cpp
+++ main.cpp
@@ -5,21 +5,64 @@
 #include <psapi.h>
 #include <winuser.h>
 #include <processthreadsapi.h>
+#include <vector>
 
 
+bool createProcess(std::string path, std::vector<std::string> &arguments, std::string working_dir) {
+    PROCESS_INFORMATION process_information;
+    ZeroMemory(&process_information, sizeof(process_information));
 
-bool createProcess(){
-    std::string exe_path = R"(C:\StorageUp\StorageUp\storageup.exe)";
-    std::string working_dir_path = R"(C:\StorageUp\StorageUp\)";
-    return CreateProcessA(exe_path, nullptr, nullptr, nullptr, false, NORMAL_PRIORITY_CLASS, nullptr, working_dir_path, );
+    std::string cmd = path;
+
+    for(int i = 0; i < arguments.size(); i++){
+        cmd += "" + arguments[i];
+    }
+
+    STARTUPINFO startupinfo;
+    ZeroMemory(&startupinfo, sizeof(startupinfo));
+    startupinfo.cb = sizeof(startupinfo);
+    auto res =  CreateProcessA(
+            path.data(),
+            cmd.data(),
+            nullptr,
+            nullptr,
+            false,
+            REALTIME_PRIORITY_CLASS,
+            nullptr,
+            working_dir.data(),
+            &startupinfo,
+            &process_information
+    );
+    std::cout << res;
 }
 
-int main() {
 
-    unsigned int start_time = clock();
-    std::cout.flush() << "Hello" << std::endl;
-    unsigned int end_time = clock();
-    std::cout << "Time spent " << end_time - start_time;
+int main() {
+    PROCESS_INFORMATION process_information;
+    ZeroMemory(&process_information, sizeof(process_information));
+
+    STARTUPINFO startupinfo;
+    ZeroMemory(&startupinfo, sizeof(startupinfo));
+    startupinfo.cb = sizeof(startupinfo);
+
+    std::string working_dir_path = R"(C:\StorageUp\StorageUp)";
+    std::string exe_path = R"(C:\StorageUp\StorageUp\ups_update.exe)";
+    std::vector<std::string> arguments = {};
+
+
+    auto res1 = createProcess(exe_path, arguments, working_dir_path);
+    printf("CreateProcess failed (%d).\n", GetLastError());
+    std::cout << "My func " << res1 << std::endl;
+
+
+//    unsigned int start_time = clock();
+//
+//    std::cout.flush() << "Hello" << std::endl;
+//
+//
+//    unsigned int end_time = clock();
+//    std::cout << "Time spent " << end_time - start_time;
+
 //    bool err = FreeConsole();
 //    using namespace std::chrono_literals;
 //    LPSTR working_dir = "C:\\StorageUp\\StorageUp\\";
