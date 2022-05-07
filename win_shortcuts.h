74b5cf5cec9744e2426aaef2d3e0f0c16413109c
alex <a99som@yandex.ru>
2022-05-07 21:02:51 +0300

    add creation shortcut and test it

--- win_shortcuts.h
+++ win_shortcuts.h
@@ -14,20 +14,20 @@
 #include <vector>
 #include <ShellScalingApi.h>
 
-extern "C" __declspec(dllexport)
-int createLinkFile(const char* path_to_exe, const char* args, const char* path_to_shortcut,
-                   const char* description, int show_mode,
-                   const char* working_dir, const char* icon_path, int icon_index);
 
 int createShortCut(LPSTR path_to_exe, LPSTR args, LPSTR path_to_shortcut,
-                    LPSTR description, int show_mode,
-                    LPSTR working_dir, LPSTR icon_path, int icon_index);
+LPSTR description, int show_mode,
+LPSTR working_dir, LPSTR icon_path, int icon_index);
 
 std::vector<std::string> split(std::string source, std::string separator);
 
 extern "C" __declspec(dllexport)
 int createProcess(const char *path, const char *arguments, const char *working_dir);
 
+extern "C" __declspec(dllexport)
+int createLinkFile(char* path_to_exe, char* args, char* path_to_shortcut,
+                   char* description, int show_mode,
+                   char* working_dir, char* icon_path, int icon_index);
 //HRESULT getScreenScale();
 
 #endif //WIN_API_WIN_SHORTCUTS_H
